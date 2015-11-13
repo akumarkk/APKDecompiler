db0c11df811592362b54dafed44fc7cf1bd1b2e2
Anil Kumar Konasale Krishna <akumarkk@stackdbquery.stackdb3.tcloud.emulab.net>
2015-11-12 23:08:04 -0700

    complete with all bug fixes

--- apkdecompiler.sh
+++ apkdecompiler.sh
@@ -1,7 +1,11 @@
 DEX2JAR_DIR="./dex2jar"
+BASEDIR=""
+LOGFILE="apkdecompiler.log"
+INSTALL_DIR=""
 
 install_dex2jar()
 {
+    cd $INSTALL_DIR;
     mkdir $DEX2JAR_DIR
     cd $DEX2JAR_DIR
 
@@ -10,11 +14,12 @@ install_dex2jar()
     
     unzip dex2jar-*
     chmod +x dex2jar-2.0/*
-    DEX2JAR_PROG=$DEX2JAR_DIR/dex2jar-*/d2j-dex2jar.sh
+    DEX2JAR_PROG=$INSTALL_DIR/$DEX2JAR_DIR/dex2jar-2.0/d2j-dex2jar.sh
 }
 
 install_apktools()
 {
+    cd $INSTALL_DIR;
     mkdir ./tmp
     cd ./tmp
 
@@ -33,6 +38,7 @@ install_apktools()
 
 install_framework_res()
 {
+    cd $INSTALL_DIR;
     wget http://qc2.androidfilehost.com/dl/iA7HYVmHYlaeIBJzKk9yTw/1447564320/23212708291677144/framework-res.apk;
 
     # If download is not successful from above link, get it from below
@@ -57,12 +63,15 @@ install_utils()
 
 extract_source()
 {
+    cd $INSTALL_DIR;
+    echo "Entered into $BASEDIR..."
     mkdir ./app
+    mkdir ./app_src
     
     # step1 : extract the apk into app_src directory
     app=$1
     cp $app ./app/
-    $DEX2JAR_PROG ./app/$app -o ./app_src/
+    $DEX2JAR_PROG ./app/$app -o ./app_src/"$app".dex2jar.jar
 
     # step2 : Decode XML files
     apktool d ./app/$app
@@ -79,10 +88,19 @@ apkdecompiler()
     fi
 
     echo "Decompiling $1..."
-
+    exec > $LOGFILE
+    exec 3>&2
+    
     install_utils;
     extract_source $1;
 
     echo "Decoded $1 into ./app_src/"
     exit 0;
 }
+
+BASEDIR=`pwd`
+INSTALL_DIR=$BASEDIR/installs
+mkdir $INSTALL_DIR
+
+echo "current working directory : $BASEDIR"
+apkdecompiler $1;
